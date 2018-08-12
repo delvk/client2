4a010ae4928ff253617eb2df3075f437c953c35a
jake nguyen <jake.nguyen@singalarity.com>
2018-08-12 22:38:49 +0700

    first commit

---
+++ custom_plugins/PluginRESTful/src/android/PluginRESTful.java
@@ -0,0 +1,194 @@
+package org.apache.cordova.plugin;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import android.content.Context;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.StringWriter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream.GetField;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class PluginRESTful extends CordovaPlugin {
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("login")) {
+            String url = args.getString(0);
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("email",args.getString(1));
+            jsonObject.put("password",args.getString(2));
+            try {
+                this.login(url, jsonObject, callbackContext);
+            } catch (Exception e) {
+                //TODO: handle exception
+                callbackContext.error(e.toString());
+            }
+            return true;
+        }
+        if (action.equals("load")) {
+            String url = args.getString(0);
+            String token = args.getString(1);
+            try {
+                this.load(url, token, callbackContext);
+            } catch (Exception e) {
+                callbackContext.error(e.toString());
+                //TODO: handle exception
+            }
+            
+            return true;
+        }
+        if (action.equals("delete")) {
+            String url = args.getString(0);
+            String token = args.getString(1);
+            try {
+                this.delete(url, token, callbackContext);
+            } catch (Exception e) {
+                callbackContext.error(e.toString());
+                //TODO: handle exception
+            }
+            
+            return true;
+        }
+        if (action.equals("signup")) {
+            String url = args.getString(0);
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("email",args.getString(1));
+            jsonObject.put("password",args.getString(2));
+            try {
+                this.signup(url, jsonObject, callbackContext);
+            } catch (Exception e) {
+                callbackContext.error(e.toString());
+                //TODO: handle exception
+            }
+            
+            return true;
+        }
+        return false;
+    }
+
+    private void login(String url, JSONObject info, CallbackContext callbackContext) throws JSONException, IOException {
+        URL server = new URL(url);
+        String input = info.toString();
+        HttpURLConnection httpURLConnection = (HttpURLConnection) server.openConnection();
+        httpURLConnection.setDoOutput(true);
+        httpURLConnection.setRequestMethod("POST");
+        httpURLConnection.setRequestProperty("Content-Type", "application/json");
+
+        OutputStream os = httpURLConnection.getOutputStream();
+        os.write(input.getBytes());
+        os.flush();
+
+        if (httpURLConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            throw new RuntimeException("Failed : HTTP error code : " + httpURLConnection.getResponseCode());
+        }
+        BufferedReader br = new BufferedReader(new InputStreamReader((httpURLConnection.getInputStream())));
+        String output;
+        while ((output = br.readLine()) != null) {
+            JSONObject jsonObject = new JSONObject(output);
+            callbackContext.success(jsonObject);
+        }
+
+        httpURLConnection.disconnect();
+
+        return;
+
+    }
+
+    private void load(String url, String token, CallbackContext callbackContext) throws IOException, JSONException {
+        URL server = new URL(url);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) server.openConnection();
+        httpURLConnection.setRequestProperty("Authorization", "bearer " + token);
+        httpURLConnection.setRequestMethod("GET");
+        httpURLConnection.setRequestProperty("Content-Type", "application/json");
+        httpURLConnection.setUseCaches(false);
+
+        if (httpURLConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            throw new RuntimeException("Failed : HTTP error code : " + httpURLConnection.getResponseCode());
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader((httpURLConnection.getInputStream())));
+        String output;
+        JSONArray jsonArray = new JSONArray();
+        while ((output = br.readLine()) != null) {
+            JSONObject jsonObject = new JSONObject(output);
+            jsonArray = jsonObject.getJSONArray("images");
+            callbackContext.success(jsonArray);
+        }
+    }
+
+    private void delete(String url, String token, CallbackContext callbackContext) throws IOException, JSONException {
+        URL server = new URL(url);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) server.openConnection();
+        httpURLConnection.setRequestProperty("Authorization", "bearer " + token);
+        httpURLConnection.setRequestMethod("DELETE");
+        httpURLConnection.setRequestProperty("Content-Type", "application/json");
+        httpURLConnection.setUseCaches(false);
+
+        if (httpURLConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            throw new RuntimeException("Failed : HTTP error code : " + httpURLConnection.getResponseCode());
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader((httpURLConnection.getInputStream())));
+
+        String output;
+        while ((output = br.readLine()) != null) {
+            callbackContext.success(output);
+        }
+    }
+
+    private void signup(String url, JSONObject info, CallbackContext callbackContext) throws JSONException, IOException {
+        URL server = new URL(url);
+        String input = info.toString();
+        HttpURLConnection httpURLConnection = (HttpURLConnection) server.openConnection();
+        httpURLConnection.setDoOutput(true);
+        httpURLConnection.setRequestMethod("POST");
+        httpURLConnection.setRequestProperty("Content-Type", "application/json");
+
+        OutputStream os = httpURLConnection.getOutputStream();
+        os.write(input.getBytes());
+        os.flush();
+
+        if (httpURLConnection.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+            throw new RuntimeException("Failed : HTTP error code : " + httpURLConnection.getResponseCode());
+        }
+
+        callbackContext.success("OK");
+        httpURLConnection.disconnect();
+        return;
+
+    }
+
+    private void echo(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success("Test successful: your message is\n" + message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+}
